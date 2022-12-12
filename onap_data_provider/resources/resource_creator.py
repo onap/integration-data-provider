d443bef37b98cbe3a7706aecbd68125755cc0045
paweldenst <pawel.denst@external.t-mobile.pl>
2022-12-12 10:32:50 +0000

    Create CPS anchor nodes using ODP
    Added: Anchor node class to cps_resource.py, registration of this class to resource_creator.py and anchor-node resource to cps.yaml
    
    Issue-ID: INT-2179
    Signed-off-by: paweldenst <pawel.denst@external.t-mobile.pl>
    Change-Id: I68c5675524c09ae6eca91fb9699b02fccdd3fd83

--- onap_data_provider/resources/resource_creator.py
+++ onap_data_provider/resources/resource_creator.py
@@ -28,7 +28,7 @@ from .cloud_region_resource import CloudRegionResource
 from .complex_resource import ComplexResource
 from .customer_resource import CustomerResource
 from .data_dictionary_resource import DataDictionarySetResource
-from .cps_resource import AnchorResource, DataspaceResource, SchemaSetResource
+from .cps_resource import AnchorNodeResource, AnchorResource, DataspaceResource, SchemaSetResource
 from .line_of_business_resource import LineOfBusinessResource
 from .msb_k8s_definition import MsbK8SDefinitionResource
 from .owning_entity_resource import OwningEntityResource
@@ -171,7 +171,11 @@ class ResourceCreator(ABC):
         },
         "cps-anchor": {
             VersionsEnum.V1_1: AnchorResource,
-            VersionsEnum.V2_0: AnchorResource
+            VersionsEnum.V2_0: AnchorResource,
+        },
+        "cps-anchor-node": {
+            VersionsEnum.V1_1: AnchorNodeResource,
+            VersionsEnum.V2_0: AnchorNodeResource
         }
     }
 
